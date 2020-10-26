280fe2b23d5b5f1d64e7c07625494ca3cd946cf3
manishs <manishs@LNAR-5CG9204ZZJ.corp.capgemini.com>
2020-10-25 21:51:47 -0500

    hi

--- src/main/java/manish/com/Spring5/controller/FilteringController.java
+++ src/main/java/manish/com/Spring5/controller/FilteringController.java
@@ -4,6 +4,7 @@ import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import manish.com.Spring5.Model.Employee;
@@ -15,7 +16,9 @@ public class FilteringController {
 	{
 		Employee emp = new Employee();
 		
-		FilterProvider filters = new SimpleFilterProvider();
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("name");
+		
+		FilterProvider filters = new SimpleFilterProvider().addFilter("beanfilter", filter);
 		
 		MappingJacksonValue mapping = new MappingJacksonValue(emp);
 		mapping.setFilters(filters);
